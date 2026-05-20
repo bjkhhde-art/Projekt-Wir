281ef6bbfc04be17f56fb1b4b686c090e2dec917
Benjamin Kupczyk <bjk.hh.de@gmail.com>
2026-05-20 16:08:40 +0200

    Create bingo.js

---
+++ bingo.js
@@ -0,0 +1,7 @@
+[
+  { "title": "6x Wandern", "done": false },
+  { "title": "Schön essen gehen", "done": false },
+  { "title": "Neues Hobby ausprobieren", "done": false },
+  { "title": "Urlaub Azoren / Lissabon", "done": false },
+  { "title": "1x Tennis spielen", "done": false }
+]
